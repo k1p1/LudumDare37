7bf969bd26e844ddb01b72eeb7d21dd742db6be9
Yasen Atanasov <y.atanasoff@gmail.com>
2016-12-10 18:05:38 +0200

    intermidiate player work

---
+++ Assets/Scripts/BumperControl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperControl : MonoBehaviour
+{
+
+    public static event Action<BumperControl> PlayerSpawned;
+    public float DashPowerup { get { return _dashTimer / DashCooldown ; } }
+    public bool CanDash { get { return Mathf.Approximately(_dashTimer, DashCooldown); } }
+
+    [SerializeField]
+    private float MoveForce;
+    [SerializeField]
+    private float HitForce;
+    [SerializeField]
+    private float DashForce;
+    [SerializeField]
+    private float DashCooldown;
+
+    private Vector3 _force = new Vector2();
+    private Rigidbody _rigidbody;
+    private float _dashTimer;
+    private bool _dash = false;
+
+    private void Start ()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        if (PlayerSpawned != null)
+        {
+            PlayerSpawned(this);
+        }
+    }
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        _force = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+        _dashTimer = Mathf.Max(_dashTimer + Time.unscaledDeltaTime, DashCooldown);
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            Dash();
+        }
+	}
+
+    private void Dash()
+    {
+        _dashTimer = 0;
+        _dash = true;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(_force * MoveForce * (_dash ? DashForce : 1.0f), ForceMode.Force);
+        _dash = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag(gameObject.tag))
+        {
+            collision.collider.GetComponent<Rigidbody>().AddForceAtPosition(-collision.contacts[0].normal * HitForce, collision.contacts[0].point, ForceMode.Impulse);
+        }
+    }
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.collider.CompareTag("Ground"))
+    //    {
+    //        _rigidbody.drag = 0;
+    //        _rigidbody.mass = 20;
+    //    }
+    //}
+}
---
+++ Assets/Scripts/CooldownUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CooldownUI : MonoBehaviour
+{
+    [SerializeField]
+    private Image CooldownImage;
+    private BumperControl player;
+	// Use this for initialization
+	void Awake ()
+    {
+       // BumperControl.PlayerSpawned += BumperSpawned;
+	}
+
+    private void BumperSpawned(BumperControl bumper)
+    {
+        player = bumper;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (player != null)
+        {
+            CooldownImage.transform.localScale = new Vector3(player.DashPowerup, 1, 1);
+        }
+    }
+}
